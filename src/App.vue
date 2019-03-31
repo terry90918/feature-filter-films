966165b9b68bce64ef4de3c8085ac370bc840545
Terry.TY Chen <zxtw17985321@gmail.com>
2019-03-31 21:34:08 +0800

    開發告個段落

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <div :class="isOnline ? 'green' : 'red'">
+      Awesome Vue.js PWA
+      <b>{{ isOnline ? "is Online" : "is Offline" }} </b>
+    </div>
+    <router-view />
+  </div>
+</template>
+
+<style lang="scss">
+html,
+body {
+  margin: 0;
+}
+</style>
