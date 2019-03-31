a7288a3721b0ceb532a07a0f5003e674aa261cf1
Terry.TY Chen <zxtw17985321@gmail.com>
2019-03-31 22:31:16 +0800

    Update Tips

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,44 @@
 <template>
   <div id="app">
     <div :class="isOnline ? 'green' : 'red'">
-      Awesome Vue.js PWA
+      App VoiceTube Demo
       <b>{{ isOnline ? "is Online" : "is Offline" }} </b>
     </div>
     <router-view />
   </div>
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      isOnline: window.navigator.onLine
+    };
+  },
+  mounted() {
+    window.addEventListener("online", () => (this.isOnline = true));
+    window.addEventListener("offline", () => (this.isOnline = false));
+  }
+};
+</script>
+
 <style lang="scss">
 html,
 body {
   margin: 0;
+}
+.green, .red {
+  border-radius: 2px;
+  color: #fff;
+  display: inline-block;
+  padding: 5px;
+  margin: 5px 0 0 5px;
+  max-width: 100%;
+}
+.green {
+  background-color: #42b983;
+}
+.red {
+  background-color: tomato;
 }
 </style>
