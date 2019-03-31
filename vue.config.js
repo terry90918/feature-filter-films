b00143876da975a19d4454f1d3754f9d67f85ec5
Terry.TY Chen <zxtw17985321@gmail.com>
2019-03-31 22:31:25 +0800

    PWA

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,9 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/app-voicetube/" : "/"
+  publicPath: process.env.NODE_ENV === "production" ? "/app-voicetube/" : "/",
+  pwa: {
+    workboxPluginMode: "InjectManifest",
+    workboxOptions: {
+      swSrc: "public/service-worker.js"
+    }
+  }
 };
